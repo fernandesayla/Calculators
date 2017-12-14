ea44d6fdd3c83e21f6d5fab1a311c90568a0c517
Ayla <fernandes.ayla@gmail.com>
2017-12-13 23:01:29 -0200

    Aula Ultima

--- app/src/main/java/com/example/logonrm/calculators/IMCFragment.java
+++ app/src/main/java/com/example/logonrm/calculators/IMCFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
@@ -18,12 +19,19 @@ public class IMCFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+    public  EditText etAltura;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_imc, container, false);
+        View v = inflater.inflate(R.layout.fragment_imc , container, false);
+
+        etAltura = (EditText) v.findViewById(R.id.etAltura);
+        return v;
+
+
+
+       // return inflater.inflate(R.layout.fragment_imc, container, false);
     }
 
 }
--- app/src/main/java/com/example/logonrm/calculators/MainActivity.java
+++ app/src/main/java/com/example/logonrm/calculators/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.logonrm.calculators;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -23,14 +26,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+      /*  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -79,23 +82,23 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_imc) {
-            // Handle the camera action
-
-        } else if (id == R.id.nav_calculadora) {
-
-
-        } else if (id == R.id.nav_medida) {
-
-        } else if (id == R.id.nav_peso) {
-
-        }else if (id == R.id.nav_temperatura) {
-
+        switch (id){
+            case R.id.nav_imc:
+                changeFragment(new IMCFragment());
         }
+
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void changeFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.content_main, fragment);
+        fragmentTransaction.commit();
     }
 }
